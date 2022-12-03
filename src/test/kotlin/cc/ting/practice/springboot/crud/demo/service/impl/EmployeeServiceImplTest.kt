7cea058636beee98e12a34136ae4badb5616c873
akechetaki10736 <chi972121@gmail.com>
2022-12-03 13:49:08 +0800

    Adding more test function to get familiar with mockMvc and how to use it

--- src/test/kotlin/cc/ting/practice/springboot/crud/demo/service/impl/EmployeeServiceImplTest.kt
+++ src/test/kotlin/cc/ting/practice/springboot/crud/demo/service/impl/EmployeeServiceImplTest.kt
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito
 
 import org.mockito.Mockito.mock
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import java.math.BigDecimal
 import java.util.*
 
@@ -22,7 +25,7 @@ class EmployeeServiceImplTest {
         val employeeDto = EmployeeDto(null, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
         val employee = employeeDto.toDo()
         BDDMockito.given(employeeDao.save(employee)).willReturn(employee.copy(1))
-        
+
         val addEmployee = employeeService.addEmployee(employeeDto)
 
         Assertions.assertThat(addEmployee.id).isNotNull
@@ -52,4 +55,59 @@ class EmployeeServiceImplTest {
         Assertions.assertThat(modifyEmployee.gender).isEqualTo(Gender.Male)
         Assertions.assertThat(modifyEmployee.balance).isEqualTo(BigDecimal(2222))
     }
+
+    @Test
+    fun queryEmployeeById() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        val employee = employeeDto.toDo()
+
+        BDDMockito.given(employeeDao.findById(1)).willReturn(Optional.of(employee))
+        val queryResult = employeeService.queryEmployeeById(1);
+
+        Assertions.assertThat(queryResult.id).isEqualTo(1)
+        Assertions.assertThat(queryResult.age).isEqualTo(26)
+        Assertions.assertThat(queryResult.name).isEqualTo("Ting, Ki")
+        Assertions.assertThat(queryResult.gender).isEqualTo(Gender.Male)
+        Assertions.assertThat(queryResult.balance).isEqualTo(BigDecimal(1000))
+        Assertions.assertThat(queryResult.language).isEqualTo("Kotlin")
+    }
+
+    @Test
+    fun queryEmployeeByFirstName() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        val employee = employeeDto.toDo()
+
+        BDDMockito.given(employeeDao.findByFirstNameOrderByIdDesc("Ting")).willReturn(listOf(employee))
+
+        val queryResult = employeeService.queryEmployeeByFirstName("Ting")
+        Assertions.assertThat(queryResult == listOf(employeeDto)).isTrue
+    }
+
+    @Test
+    fun queryEmployeeByLastName() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        val employee = employeeDto.toDo()
+
+        BDDMockito.given(
+            employeeDao.findByLastName("Ki", PageRequest.of(1, 1, Sort.Direction.DESC, "id"))
+        ).willReturn(PageImpl(listOf(employee)))
+
+        val queryResult = employeeService.queryEmployeeByLastName("Ki", PageRequest.of(1, 1, Sort.Direction.DESC, "id"))
+        Assertions.assertThat(queryResult == PageImpl(listOf(employeeDto))).isTrue
+    }
+
+    @Test
+    fun modifyEmployeeWithAge() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        val employee = employeeDto.toDo()
+        val newEmployeeDto = employeeDto.copy(age = 100)
+        val newEmployee = newEmployeeDto.toDo()
+        
+        BDDMockito.given(employeeDao.findById(1)).willReturn(Optional.of(employee))
+        BDDMockito.given(employeeDao.save(newEmployee)).willReturn(newEmployee)
+        
+        val updateResult = employeeService.modifyEmployeeWithAge(newEmployeeDto)
+        Assertions.assertThat(updateResult).isEqualTo(newEmployeeDto)
+    }
+
 }
--- src/test/kotlin/cc/ting/practice/springboot/crud/demo/web/controller/EmployeeControllerTest.kt
+++ src/test/kotlin/cc/ting/practice/springboot/crud/demo/web/controller/EmployeeControllerTest.kt
@@ -2,16 +2,24 @@ package cc.ting.practice.springboot.crud.demo.web.controller
 
 import cc.ting.practice.springboot.crud.demo.data.dto.EmployeeDto
 import cc.ting.practice.springboot.crud.demo.data.enu.Gender
+import cc.ting.practice.springboot.crud.demo.data.vo.EmployeeVo
 import cc.ting.practice.springboot.crud.demo.service.EmployeeService
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.math.BigDecimal
 
 @WebMvcTest(EmployeeController::class)
@@ -27,13 +35,106 @@ class EmployeeControllerTest {
 
     @Test
     fun addEmployee() {
-
         val employeeDto = EmployeeDto(null, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        BDDMockito.given(employeeService.addEmployee(employeeDto)).willReturn(employeeDto.copy(id = 1))
 
         mockMvc.perform(
             post("/employees")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(employeeDto))
         )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().string(objectMapper.writeValueAsString(employeeDto.copy(id = 1))))
     }
+
+    @Test
+    fun queryEmployeeById() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        BDDMockito.given(employeeService.queryEmployeeById(1)).willReturn(employeeDto)
+
+        mockMvc.perform(
+            get("/employees/{id}", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().string(objectMapper.writeValueAsString(employeeDto)))
+    }
+
+    @Test
+    fun queryEmployeeByFirstName() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        BDDMockito.given(employeeService.queryEmployeeByFirstName("Ting")).willReturn(listOf(employeeDto))
+
+        mockMvc.perform(
+            get("/employees/firstName/{firstName}", "Ting")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().string(objectMapper.writeValueAsString(listOf(employeeDto))))
+    }
+
+    @Test
+    fun queryEmployeeByLastName() {
+        val employeeDto = EmployeeDto(1, "Ting, Ki", 26, "Kotlin", Gender.Male, BigDecimal(1000), null)
+        BDDMockito.given(
+            employeeService.queryEmployeeByLastName("Ki", PageRequest.of(1, 1, Sort.Direction.DESC, "id"))
+        ).willReturn(PageImpl(listOf(employeeDto)))
+
+        mockMvc.perform(
+            get("/employees")
+                .param("lastName", "Ki")
+                .param("page", "1")
+                .param("size", "1")
+                .param("column", "id")
+                .param("direction", "DESC")
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().string(objectMapper.writeValueAsString(PageImpl(listOf(employeeDto)))))
+    }
+
+    @Test
+    fun modifyEmployee() {
+        val newEmployeeDto = EmployeeDto(1, "Ting, Ki", 30, "Kotlin", Gender.Male, BigDecimal(3333), null)
+        BDDMockito.given(employeeService.modifyEmployee(newEmployeeDto)).willReturn(newEmployeeDto.copy())
+
+        mockMvc.perform(
+            put("/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newEmployeeDto))
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().string(objectMapper.writeValueAsString(newEmployeeDto)))
+    }
+
+    @Test
+    fun modifyEmployeeAgeById() {
+        val employeeVo = EmployeeVo(1, 100)
+        BDDMockito.willDoNothing().given(employeeService).modifyEmployeeAgeById(100, 1)
+
+        mockMvc.perform(
+            patch("/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employeeVo))
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun removeEmployeeById() {
+        BDDMockito.willDoNothing().given(employeeService).removeEmployeeById(1)
+
+        mockMvc.perform(
+            delete("/employees/{id}", 1)
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+    }
+
+
 }
